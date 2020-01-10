a819b4dc8b2cfcdbaee3557916bb3da342fb684a
Vitalii Agapov <vitaly.agapov@outlook.com>
2020-01-10 15:27:10 -0500

    updated getDetails

--- src/NameDetail.js
+++ src/NameDetail.js
@@ -7,53 +7,80 @@ export default class nameDetail extends Component {
         details: undefined,
     }
 
-// componentDidMount() {
-//     this.getDetails()
-// }
+    // componentDidMount() {
+    //     this.getDetails()
+    // }
 
-componentWillReceiveProps() {
+    componentWillReceiveProps() {
 
-    this.getDetails()
+        this.getDetails()
 
-    this.setState({
-        location: this.props.location
-    })
-}
+    }
 
 
     // componentDidUpdate() {
 
     //     this.getDetails()
-    
+
     //   }
 
-    getDetails = async (e) => {
-        const urlDetail = this.props.match.url
-        const apiUrl = `https://swapi.co/api`
-        console.log('URL', this.state.location)
 
-        console.log('calling get Names function')
-        let request = axios.get(`${apiUrl}${urlDetail}`)
-          .then(response => {
-            const data = response.data;
-            console.log('data', data);
-    
-            this.setState({
-              details: data.results
-            })
-    
-          })
-          .catch(error => {
-            console.error('create student error', error.response)
-            // alert(JSON.stringify(error.response.data))
-          })
-      }
+    //   .then(response => {
+    // const data = response.data;
+    // console.log('data', data);
 
-      showDetails = () => {
-       if (this.state.details) {
+    // this.setState({
+    //   details: data.results
+    // })
+
+
+
+    //   })
+    //   .catch(error => {
+    //     console.error('create student error', error.response)
+    //     // alert(JSON.stringify(error.response.data))
+    //   })
+
+    getDetails = async e => {
+        const urlDetail = this.props.match.url;
+        const apiUrl = `https://swapi.co/api`;
+        console.log('URL', this.state.location);
+        console.log('calling get Names function');
+        try {
+            let request = await axios.get(`${apiUrl}${urlDetail}`);
+            console.log('request', request.data);
+            this.setState({ details: request.data.results });
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    // getDetails = async (e) => {
+    //     const urlDetail = this.props.match.url
+    //     const apiUrl = `https://swapi.co/api`
+    //     console.log('URL', this.state.location)
+
+    //     console.log('calling get Names function')
+    //     let request = await axios.get(`${apiUrl}${urlDetail}`)
+
+    //     const data = await request.json();
+
+    //     console.log(data)
+
+    //     // try {
+    //     //     this.setState({details: request.data.results})
+    //     //     console.log('details', this.state.details)
+    //     //     console.log('props', this.props)
+    //     //     } catch (error) {
+    //     //     console.log(error)
+    //     //     } 
+    // }
+
+    showDetails = () => {
+        if (this.state.details) {
             console.log(this.state.details)
-       }
-      
+        }
+
     }
 
     render() {
@@ -68,35 +95,35 @@ componentWillReceiveProps() {
         console.log()
 
 
-            return (
-                <div>
-    
-                    <div className="col-7">
-                        <h1></h1>
-                        
-                        <table className="table">
-                            <thead></thead>
-                            <tbody>
-                                <tr>
-                                    <td>Capital</td>
+        return (
+            <div>
 
-                                </tr>
-                                <tr>
-                                    <td>Area</td>
+                <div className="col-7">
+                    <h1></h1>
 
-                        <sup>2</sup>
+                    <table className="table">
+                        <thead></thead>
+                        <tbody>
+                            <tr>
+                                <td>Capital</td>
 
-                                </tr>
-                                <tr>
-                                    <td>Borders</td>
-                                    <td>
+                            </tr>
+                            <tr>
+                                <td>Area</td>
+
+                                <sup>2</sup>
+
+                            </tr>
+                            <tr>
+                                <td>Borders</td>
+                                <td>
                                     {this.showDetails()}
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
-            )
+            </div>
+        )
     }
 }
