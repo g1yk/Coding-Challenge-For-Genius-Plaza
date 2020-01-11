0f524cd50c85c7ef30fca25b9c839f7132d37631
Vitalii Agapov <vitaly.agapov@outlook.com>
2020-01-11 17:25:22 -0500

    fixed nameDetail

--- src/NameDetail.js
+++ src/NameDetail.js
@@ -4,97 +4,28 @@ import axios from 'axios'
 export default class nameDetail extends Component {
 
     state = {
-        details: undefined,
+        details: {},
+        url: '',
     }
 
-    // componentDidMount() {
-    //     this.getDetails()
-    // }
-
-    componentWillReceiveProps() {
-
-        this.getDetails()
-
+    componentDidMount() {
+        this.getDetails();
     }
 
-
-    // componentDidUpdate() {
-
-    //     this.getDetails()
-
-    //   }
-
-
-    //   .then(response => {
-    // const data = response.data;
-    // console.log('data', data);
-
-    // this.setState({
-    //   details: data.results
-    // })
-
-
-
-    //   })
-    //   .catch(error => {
-    //     console.error('create student error', error.response)
-    //     // alert(JSON.stringify(error.response.data))
-    //   })
-
-    getDetails = async e => {
-        const urlDetail = this.props.match.url;
+    getDetails = async () => {
         const apiUrl = `https://swapi.co/api`;
-        console.log('URL', this.state.location);
-        console.log('calling get Names function');
-        try {
-            let request = await axios.get(`${apiUrl}${urlDetail}`);
-            console.log('request', request.data);
-            this.setState({ details: request.data.results });
-        } catch (error) {
-            console.log(error);
-        }
+        let response = await axios.get(`${apiUrl}${this.props.match.url}`);
+        this.setState({details: response.data, url: this.props.match.url});
+        console.log(this.state.details)          
     };
 
-    // getDetails = async (e) => {
-    //     const urlDetail = this.props.match.url
-    //     const apiUrl = `https://swapi.co/api`
-    //     console.log('URL', this.state.location)
-
-    //     console.log('calling get Names function')
-    //     let request = await axios.get(`${apiUrl}${urlDetail}`)
-
-    //     const data = await request.json();
-
-    //     console.log(data)
-
-    //     // try {
-    //     //     this.setState({details: request.data.results})
-    //     //     console.log('details', this.state.details)
-    //     //     console.log('props', this.props)
-    //     //     } catch (error) {
-    //     //     console.log(error)
-    //     //     } 
-    // }
-
-    showDetails = () => {
-        if (this.state.details) {
-            console.log(this.state.details)
-        }
-
-    }
-
     render() {
-        // console.log('resutls', this.props)
-        // console.log('url', this.props.match.url)
-
-        // console.log('state', this.state)
-
-        const urlDetail = this.props.match.url
-        const apiUrl = `https://swapi.co/api`
-
-        console.log()
-
-
+        if (this.state.url !== this.props.match.url) {
+            this.getDetails();
+        }
+      
+        const { height, name, mass, birth_year } = this.state.details;
+        
         return (
             <div>
 
@@ -104,21 +35,22 @@ export default class nameDetail extends Component {
                     <table className="table">
                         <thead></thead>
                         <tbody>
-                            <tr>
-                                <td>Capital</td>
 
+                            <tr>
+                                <td><b>Name</b> </td>
+                                <td>{name}</td>
                             </tr>
                             <tr>
-                                <td>Area</td>
-
-                                <sup>2</sup>
-
+                                <td>Height </td>
+                                <td> {height} </td>
                             </tr>
                             <tr>
-                                <td>Borders</td>
-                                <td>
-                                    {this.showDetails()}
-                                </td>
+                                <td>Mass </td>
+                                <td>{mass}</td>
+                            </tr>
+                            <tr>
+                                <td>Birth year </td>
+                                <td>{birth_year}</td>
                             </tr>
                         </tbody>
                     </table>
