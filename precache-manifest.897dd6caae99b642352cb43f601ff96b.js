a5880c8aa8781bd70c5ab2af5bd486b4c0e63cf2
Vitalii Agapov <vitaly.agapov@outlook.com>
2020-01-11 17:31:22 -0500

    Updates

---
+++ precache-manifest.897dd6caae99b642352cb43f601ff96b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "767b7fe2054ff84a53961c091126a7de",
+    "url": "/Coding-Challenge-For-Genius-Plaza/index.html"
+  },
+  {
+    "revision": "34045da701f2b77c7775",
+    "url": "/Coding-Challenge-For-Genius-Plaza/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "865f003b083977760624",
+    "url": "/Coding-Challenge-For-Genius-Plaza/static/css/main.3942478a.chunk.css"
+  },
+  {
+    "revision": "34045da701f2b77c7775",
+    "url": "/Coding-Challenge-For-Genius-Plaza/static/js/2.71786896.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/Coding-Challenge-For-Genius-Plaza/static/js/2.71786896.chunk.js.LICENSE"
+  },
+  {
+    "revision": "865f003b083977760624",
+    "url": "/Coding-Challenge-For-Genius-Plaza/static/js/main.5cb69ae9.chunk.js"
+  },
+  {
+    "revision": "d4c6c18cd060ac278ce3",
+    "url": "/Coding-Challenge-For-Genius-Plaza/static/js/runtime-main.eefdfa8c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Coding-Challenge-For-Genius-Plaza/static/media/logo.5d5d9eef.svg"
+  }
+]);
